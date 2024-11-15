b46b9a9db521abd8df78017ca49c3d9a58c9923a
maogualaedu <4b3g0123@office.stust.edu.tw>
2024-11-15 23:58:35 +0800

    add note

---
+++ loop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int a_for(){
+
+return 0;
+}
+int b_while(){
+
+return 0;
+}
+int c_do_while(){
+
+return 0;
+}
+
+//main aera.
+int main(){
+
+std::string sel;
+int x,y;
+
+std::cout<<"Please enter the choise(a \\ b \\ c): ";
+std::cin>>sel;
+
+return 0;
+
+}
