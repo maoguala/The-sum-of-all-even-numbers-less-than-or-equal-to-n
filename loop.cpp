07548be68e96a300dcf7e12967e05aed86db0b4b
maogualaedu <4b3g0123@office.stust.edu.tw>
2024-11-18 01:06:12 +0800

    version++

--- loop.cpp
+++ loop.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
 
-int a_for(){
+int a_for(int z = 0,int s = 0){
+    //z = x;
+for(int i = 1;i <=z; i++){
+        if(i%2 == 0){
+            s = s + i;
+        }
+    }
+    std::cout << "Totall: " << s << std::endl;
+return 0;
+}
+int b_while(int z = 0,int s = 0){
 
 return 0;
 }
-int b_while(){
-
-return 0;
-}
-int c_do_while(){
+int c_do_while(int z = 0,int s = 0){
 
 return 0;
 }
@@ -18,11 +24,32 @@ return 0;
 int main(){
 
 std::string sel;
-int x,y;
+int x,y = 0;
+std::cout << "Please enter the number: ";
+std::cin >> x;
+std::cout << "Please enter the choise(a \\ b \\ c): ";
 
-std::cout<<"Please enter the choise(a \\ b \\ c): ";
+while(1){
+
 std::cin>>sel;
 
-return 0;
+if(sel == "a" || sel == "A"){
+
+    return a_for(x,y);
+
+}else if(sel == "b" || sel == "B"){
+    return b_while(x,y);
+
+}else if(sel == "c" || sel == "C"){
+    return c_do_while(x,y);
+}else{
+ 
+    std::cout<<"Please re-enter the choise: ";
 
 }
+}
+
+return 0;
+}
+
+
