05a75573a1d45289d4bbfc8d25e3e4a02f65d68e
mao <92989307+maoguala@users.noreply.github.com>
2024-11-18 23:04:06 +0800

    Update loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -1,28 +1,71 @@
 #include <iostream>
 #include <string>
 
-int a_for(){
-
-return 0;
+int a_for(int z = 0,int s = 0){
+    
+    for(int i = 1;i <=z; i++){
+        if(i%2 == 0){
+            s = s + i;
+        }
+    }
+    std::cout << "Totall: " << s << std::endl;
+    return 0;
 }
-int b_while(){
-
-return 0;
+int b_while(int z = 0,int s = 0){
+    
+    int i = 0;
+    while (i <= z)
+    {
+        if(i%2 == 0){
+            s = s + i;
+        }
+        i++;
+    }
+    std::cout << "Totall: " << s << std::endl;
+    return 0;
 }
-int c_do_while(){
-
-return 0;
+int c_do_while(int z = 0,int s = 0){
+    
+    int i = 0;
+    do{
+        if(i%2 == 0){
+            s = s + i;
+        }
+        i++;
+    }while (i <= z);
+    std::cout << "Totall: " << s << std::endl;
+    return 0;
 }
 
 //main aera.
 int main(){
 
 std::string sel;
-int x,y;
+int x,y = 0;
+std::cout << "Please enter the number: ";
+std::cin >> x;
+std::cout << "Please enter the choise(a \\ b \\ c): ";
 
-std::cout<<"Please enter the choise(a \\ b \\ c): ";
+while(1){
+
 std::cin>>sel;
 
-return 0;
+if(sel == "a" || sel == "A"){
+
+    return a_for(x,y);
+
+}else if(sel == "b" || sel == "B"){
+    return b_while(x,y);
+
+}else if(sel == "c" || sel == "C"){
+    return c_do_while(x,y);
+}else{
+ 
+    std::cout<<"Please re-enter the choise: ";
 
 }
+}
+
+return 0;
+}
+
